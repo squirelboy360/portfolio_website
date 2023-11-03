aa3d6f5c922884b0edb5dd5514f1ba170dae66db
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2023-11-03 15:02:07 +0100

    ui+logic voltooid

--- scripts/home_scripts.js
+++ scripts/home_scripts.js
@@ -10,3 +10,13 @@ window.onscroll = function(){
         header.classList.remove('navBarDark');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+  
+    menuToggle.addEventListener('click', function () {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
